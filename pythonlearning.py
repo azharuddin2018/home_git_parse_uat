00f1d836cb38f06a8b2742a87be4a85d108bef71
Azhar Udin <azharudin@Azhars-MacBook-Pro.local>
2024-08-24 18:00:03 +0530

    Update pythonlearning.py

--- pythonlearning.py
+++ pythonlearning.py
@@ -1,66 +1,106 @@
-# movies = []
-# mov1 = input("Enter First Movie: ")
-# mov2 = input("Enter Second Movie: ")
-# mov3 = input("Enter Third Movie: ")
+# # # movies = []
+# # # mov1 = input("Enter First Movie: ")
+# # # mov2 = input("Enter Second Movie: ")
+# # # mov3 = input("Enter Third Movie: ")
 
 
-# ##We can directly take input into movies.append()
-# movies.append(mov1)
-# movies.append(mov2)
-# movies.append(mov3)
+# # # ##We can directly take input into movies.append()
+# # # movies.append(mov1)
+# # # movies.append(mov2)
+# # # movies.append(mov3)
 
-# print(movies)
-
-
-#list = [1, 2, 1]
-
-#  input using a loop  
+# # # print(movies)
 
 
+# # #list = [1, 2, 1]
 
-# n = int(input("Enter Number of Value :"))
-
-# list = []
-
-# for i in range(n):
-#     element = input("Enter element {}: ".format(i+1))
-# list.append(element)
-
-# print("List:",list)
+# # #  input using a loop  
 
 
-# copy_list = list.copy()
-# copy_list.reverse()
-# if(copy_list == list):
-#      print("palindrome")
-# else:
-#      print("not palindrome")
 
-#  input using a loop  
-# creating an empty list
-# Code to take input of a list
-# from the user.
+# # # n = int(input("Enter Number of Value :"))
 
-# Take input of the size of the list
-n = int (input ("Enter number of elements: ")) 
+# # # list = []
 
-# Declare an empty list
-list = []
+# # # for i in range(n):
+# # #     element = input("Enter element {}: ".format(i+1))
+# # # list.append(element)
 
-# Iterate for n times take inputs
-for i in range (n):
-    # Take input of ith element as x.
-    x = int(input())
-    # Append 'x' to the list.
-    list.append(x)
-
-# Print the list
-#print("List:", list)
+# # # print("List:",list)
 
 
-copy_list = list.copy()
-copy_list.reverse()
-if(copy_list == list):
-      print("It is a palindrome")
-else:
-      print("not palindrome")
+# # # copy_list = list.copy()
+# # # copy_list.reverse()
+# # # if(copy_list == list):
+# # #      print("palindrome")
+# # # else:
+# # #      print("not palindrome")
+
+# # #  input using a loop  
+# # # creating an empty list
+# # # Code to take input of a list
+# # # from the user.
+
+# # # Take input of the size of the list
+# # n = int (input ("Enter number of elements: ")) 
+
+# # # Declare an empty list
+# # list = []
+
+# # # Iterate for n times take inputs
+# # for i in range (n):
+# #     # Take input of ith element as x.
+# #     x = int(input())
+# #     # Append 'x' to the list.
+# #     list.append(x)
+
+# # # Print the list
+# # #print("List:", list)
+
+
+# # copy_list = list.copy()
+# # copy_list.reverse()
+# # if(copy_list == list):
+# #       print("It is a palindrome")
+# # else:
+# #       print("not palindrome")
+
+
+# info = {
+
+#     "name" : "Azhar",
+#     "subjects" : ["python", "etl", "oracle"]
+
+# }
+
+# print(info)
+
+# info["name"] = "Mohammad"
+# info["surname"] = "Azhar Uddin"
+# print(info)
+
+
+# null_dict = {}
+# null_dict["key"] = "value"
+# print(null_dict)
+
+
+#nested dictionary
+student = {
+    "name" : "azhar",
+    "subjects" : {
+        "physics":80, "maths":90, "science":99
+        }
+}
+
+#print(student["subjects"]["maths"])
+
+#print(len(list(student.keys())))
+
+# print(list(student.values()))
+
+# pairs = list(student.items())
+# print(pairs[0])
+
+print(student["name"]) ##error while using square brackets for any non-existing variable
+print(student.get("name")) ##no error when using function to call a non-existing variable
